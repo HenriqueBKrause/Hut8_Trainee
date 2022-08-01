731adbd7f79fc60674e8ba6bd698612c883ec47b
Henrique Krause <henriquebkrause2312@gmail.com>
2022-08-01 10:18:07 -0300

    Hut8

---
+++ semana_2/exerciciodeaula2.js
@@ -0,0 +1,8 @@
+//Crie um algoritmo que imprime todos os valores de um array qualquer,menosoultimo elemento.
+
+
+let arreyvolue = [1, 2, 3, 4, 5, 6, 7, 8];
+
+for(let i=0; i < arreyvolue.length-1; i++){
+    console.log(arreyvolue[i]);
+}
