8b781c1943c22a4d7047c342a1d8fd24e709d7c3
Henrique Krause <henriquebkrause2312@gmail.com>
2022-08-01 09:25:23 -0300

    Hut8

---
+++ semana_2/checkpoints/checkpoint3.js
@@ -0,0 +1,5 @@
+//Crie um algoritmo que tem como entrada um array de números e 
+//imprime no console a soma dos elementos sendo cada um deles multiplicado pelo seu índice.
+//Exemplo entrada: [5, 9, 10, 6]
+//Exemplo Saída: 47
+
