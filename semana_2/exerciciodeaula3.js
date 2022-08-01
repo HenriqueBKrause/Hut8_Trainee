ae15acf54d65b4981f914c21b9268b1c6374d403
Henrique Krause <henriquebkrause2312@gmail.com>
2022-08-01 15:21:14 -0300

    Hut8

---
+++ semana_2/exerciciodeaula3.js
@@ -0,0 +1,24 @@
+// Crie um algoritmo que tem como entrada um objeto e imprime no
+// console os nomes dos projetos ativos do usuário.
+
+const user = [
+    {
+        name: "Henrique Borges Krause",
+        project:[
+            { name: "Processo seletivo", start: "23/12/2002"}
+        ]
+    },
+    {
+        name: "Fernando Alvarenga",
+        project:[
+            { name: "Hut8", start: "01/08/2022"}
+        ]
+    },
+    {
+        name: "Gabriel Toledo",
+        project:[
+            { name: "Formação Otterwise", start: "01/08/2022"}
+        ]
+    }
+]
+console.log(user[0]);
