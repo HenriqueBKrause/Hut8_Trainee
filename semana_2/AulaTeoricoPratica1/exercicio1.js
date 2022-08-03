074d2ee7728d9377a7889853ca37ab1cc310b38c
Henrique Krause <henriquebkrause2312@gmail.com>
2022-08-03 10:08:18 -0300

    Hut8

---
+++ semana_2/AulaTeoricoPratica1/exercicio1.js
@@ -0,0 +1,9 @@
+//Imprima no console os valores de 1 até 25.
+
+console.log("A sequencia de numero é 1 até 25");
+
+for (let i = 1; i <= 25; i++) {
+    console.log(i);
+}
+console.log("Obrigado por usar o programa!");
+
