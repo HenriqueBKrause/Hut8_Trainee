6945959dc648da264d653e71d50d0c211470a91b
Henrique Krause <henriquebkrause2312@gmail.com>
2022-08-03 10:11:00 -0300

    Hut8

---
+++ semana_2/AulaTeoricoPratica1/exercicio2.js
@@ -0,0 +1,10 @@
+//Imprima no console os valores de 10 até 200 de10 em 10
+
+console.log("A sequencia de numero é 10 até 200 de10 em 10");
+
+    for (let i = 10; i <= 200; i += 10) {
+        console.log(i);
+    }
+
+
+    console.log("Obrigado por usar o programa!");
