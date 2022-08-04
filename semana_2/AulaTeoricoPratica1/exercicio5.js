aa0a8e41c2102515225ffe998c97169663a7740d
Henrique Krause <henriquebkrause2312@gmail.com>
2022-08-04 15:37:58 -0300

    Hut8

---
+++ semana_2/AulaTeoricoPratica1/exercicio5.js
@@ -0,0 +1,5 @@
+//Dado um array de números, faça um programa queimprima na tela somenteos números positivos.
+
+
+let number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15];
+console.log(number.filter(number => number > 0));
