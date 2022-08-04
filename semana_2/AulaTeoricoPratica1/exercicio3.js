d43b7a273896ac0b7c7302b0c1bebc315442d987
Henrique Krause <henriquebkrause2312@gmail.com>
2022-08-04 14:47:49 -0300

    Hut8

---
+++ semana_2/AulaTeoricoPratica1/exercicio3.js
@@ -0,0 +1,9 @@
+//Dado um array de nomes, faça um programa que imprimana tela todos os nomes (na mesma linha).
+
+
+let array = ['Angela', 'Rosa', 'Ticiana', 'Carla', 'Renata'];
+
+for (let i = 0; i < array.length; i++) {
+    console.log(array[i]);
+}
+console.log("Obrigado por usar o programa!");
