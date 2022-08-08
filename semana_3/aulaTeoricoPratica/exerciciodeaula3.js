f774a167b1c0493c60b236f9d910ae9feca605d4
Henrique Krause <henriquebkrause2312@gmail.com>
2022-08-08 13:02:51 -0300

    Hut8

---
+++ semana_3/aulaTeoricoPratica/exerciciodeaula3.js
@@ -0,0 +1,9 @@
+// dado um array de nomes, faça umprograma que imprima natela todos os nomes (na mesma linha).
+
+//inicio do codigo
+
+const name = ['Angela', 'Rosa', 'Ticiana', 'Carla', 'Renata'];
+
+//processamento
+console.log(name);
+
