9364db8c98c10ae074fe7398ad78b965d71a325d
Henrique Krause <henriquebkrause2312@gmail.com>
2022-08-16 18:37:14 -0300

    Hut8

--- semana_3/aulaTeoricoPratica/exerciciodeaula1.js
+++ semana_3/aulaTeoricoPratica/exerciciodeaula1.js
@@ -1,24 +1,24 @@
-//Pegue um valor de entrada e calculeo menor número denotas possíveis (cédulas) no qual o valor pode ser decomposto. 
+//Pegue um valor de entrada e calculeo menor número denotas possíveis (cédulas) no qual o valor pode ser decomposto.
 //As notasconsideradas são de 100, 50, 20, 10, 5, 2 e 1. A seguir mostre o valor lido e arelação de notas necessárias
 
 //inicio do codigo
-const valorimput = require('prompt-sync')();
+const valorimput = require("prompt-sync")();
 
 //entrada do valor
 console.log("Digite o valor: ");
 let valor = valorimput();
 
 //processamento
-function notasdisponiveis(valor){
-    let notas = [100, 50, 20, 10, 5, 2, 1];
-    let notasdisponiveis = [];
-    for(let i = 0; i < notas.length; i++){
-        while(valor >= notas[i]){
-            valor -= notas[i];
-            notasdisponiveis.push(notas[i]);
-        }
+function notasdisponiveis(valor) {
+  let notas = [100, 50, 20, 10, 5, 2, 1];
+  let notasdisponiveis = [];
+  for (let i = 0; i < notas.length; i++) {
+    while (valor >= notas[i]) {
+      valor -= notas[i];
+      notasdisponiveis.push(notas[i]);
     }
-    return notasdisponiveis;
+  }
+  return notasdisponiveis;
 }
 
 //saida
